52c35f9a5011b75d1f279e0069413fe162c16e25
NakagamiYuta <guguru0014@gmail.com>
2020-09-22 06:19:20 +0900

    [add] 再生速度設定スライダーが動くようにした

--- src/controller/MainUIController.java
+++ src/controller/MainUIController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainUIController implements Initializable {
+
+    // UI部品
+    @FXML
+    private Slider speedSlider;
+    @FXML
+    private Label speedVal;
 
     // 描画用
     private double updateSpeed;
@@ -19,6 +28,18 @@ public class MainUIController implements Initializable {
         widthTVal = 25;
         heightTVal = 10;
         widthFVal = heightFVal = 0;
+
+        // その他色々初期化
+        initUI();
+    }
+
+    /* UI初期化 */
+    private void initUI() {
+        // UIイベント<スライダー>
+        speedSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            updateSpeed = Math.round(oldVal.doubleValue()*10)/10.0;
+            speedVal.setText("x"+updateSpeed);
+        });
     }
 
 }
