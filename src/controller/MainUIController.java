a59eed4767601a265c8a490db70f5f1747062c56
NakagamiYuta <guguru0014@gmail.com>
2020-09-22 06:15:27 +0900

    [add] UIの値を持つ変数を初期化するようにした

--- src/controller/MainUIController.java
+++ src/controller/MainUIController.java
@@ -7,9 +7,18 @@ import java.util.ResourceBundle;
 
 public class MainUIController implements Initializable {
 
+    // 描画用
+    private double updateSpeed;
+    private double widthFVal, widthTVal, heightFVal, heightTVal;
+
     /* 初期化 */
     @Override
     public void initialize(URL location, ResourceBundle resource) {
+        // 各種値初期化
+        updateSpeed = 1.0;
+        widthTVal = 25;
+        heightTVal = 10;
+        widthFVal = heightFVal = 0;
     }
 
 }
