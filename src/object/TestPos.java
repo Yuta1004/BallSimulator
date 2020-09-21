3a7467629f5525f18a449419c4bbf04b97802142
NakagamiYuta <guguru0014@gmail.com>
2020-09-22 03:20:01 +0900

    [add] Pos.changePos実装

--- src/object/TestPos.java
+++ src/object/TestPos.java
@@ -10,7 +10,8 @@ public class TestPos extends Test {
 
     public void test() {
         initializeTest();
-        changeValTest();
+        updatePosTest();
+        changePosTest();
         resetTest();
     }
 
@@ -21,18 +22,25 @@ public class TestPos extends Test {
     }
 
     /* 値増減テスト(※誤差によるテスト失敗を防ぐために整数値での増減) */
-    private void changeValTest() {
+    private void updatePosTest() {
         double changesX[] = {-78, 8, 2, -94, -38};
         double changesY[] = {77, -63, -13, 54, -52};
         double correctX[] = {22, 30, 32, -62, -100};
         double correctY[] = {177, 114, 101, 155, 103};
         Pos pos = new Pos(100, 100);
         for(int cnt = 0; cnt < 5; ++ cnt) {
-            pos.x += changesX[cnt];
-            pos.y += changesY[cnt];
+            pos.updatePos(changesX[cnt], changesY[cnt]);
             isEqual(pos.x, correctX[cnt]);
             isEqual(pos.y, correctY[cnt]);
         }
+    }
+
+    /* 座標変更テスト */
+    private void changePosTest() {
+        Pos pos = new Pos(0, 0);
+        isEqual(pos, new Pos(0, 0));
+        pos.changePos(100, 100);
+        isEqual(pos, new Pos(100, 100));
     }
 
     /* リセットテスト */
