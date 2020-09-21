345e99e734c76891b973267da98f05480a173322
NakagamiYuta <guguru0014@gmail.com>
2020-09-22 02:56:07 +0900

    [add] SimulatableObject.giveVelocity, forceに関数コメントを追加

--- src/object/SimulatableObject.java
+++ src/object/SimulatableObject.java
@@ -31,9 +31,18 @@ public abstract class SimulatableObject {
     /* 状態のリセットを行う */
     public abstract void reset();
 
+    /**
+     * 速度を与える
+     * @param vx x方向の速度 [m/s]
+     * @param vy y方向の速度 [m/s]
+     */
     public abstract void giveVelocity(double vx, double vy);
 
-    /* 力を加える */
+    /**
+     * 力を加える
+     * @param fx x方向の力 [N]
+     * @param fy y方向の力 [N]
+     */
     public abstract void force(double fx, double fy);
 
 }
