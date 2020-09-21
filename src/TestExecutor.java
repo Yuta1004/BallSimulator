41f03f1fac796c01da52cc476ebac060ec224911
NakagamiYuta <guguru0014@gmail.com>
2020-09-22 01:53:02 +0900

    [add] テスト全成功時はメッセージを出力するようにした

--- src/TestExecutor.java
+++ src/TestExecutor.java
@@ -20,6 +20,9 @@ class TestExecutor {
         System.out.println("---Test Results---");
         System.out.println("> Success: "+Test.cnt[Test.SUCCESS]);
         System.out.println("> Failed : "+Test.cnt[Test.FAILED]);
+        if(Test.cnt[Test.FAILED] == 0) {
+            System.out.println("Congratulations!\nAll of tests are success!");
+        }
     }
 
 }
