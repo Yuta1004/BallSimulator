69e210a17bbb2befeed698bee82f35ab6dc66c8c
NakagamiYuta <guguru0014@gmail.com>
2020-09-22 22:14:51 +0900

    [add] data.Settingsクラスを書いた
    データクラス

---
+++ src/data/Settings.java
@@ -0,0 +1,10 @@
+package data;
+
+public class Settings {
+
+    public static double RockMagnification = 1.0;
+    public static boolean ViewRatioNormalize = true;
+    public static boolean AxisNormalize = false;
+    public static double StepVal = 0.1;
+
+}
