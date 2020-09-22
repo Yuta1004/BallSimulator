17d16b06871aee5d2f91f94cb86478059b6c4675
NakagamiYuta <guguru0014@gmail.com>
2020-09-22 22:15:16 +0900

    [add] SettingsController実装

---
+++ src/controller/SettingsController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import util.Parse;
+import data.Settings;
+
+public class SettingsController implements Initializable {
+
+    // UI部品
+    @FXML
+    private Label objMagValueV;
+    @FXML
+    private Slider objMagValue;
+    @FXML
+    private CheckBox viewRatioNormalize, axisNormalize;
+    @FXML
+    private TextField stepVal;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resource) {
+        // 初期化(UI部品)
+        stepVal.setText(""+Settings.StepVal);
+        objMagValue.setValue(Settings.RockMagnification);
+        objMagValueV.setText("x "+Settings.RockMagnification);
+        axisNormalize.setSelected(Settings.AxisNormalize);
+        viewRatioNormalize.setSelected(Settings.ViewRatioNormalize);
+
+        // 画面表示の拡大率
+        objMagValue.valueProperty().addListener((obs, oldVal, newVal) -> {
+            double val = Math.round(newVal.doubleValue()*10)/10.0;
+            objMagValueV.setText("x "+val);
+            Settings.RockMagnification = val;
+        });
+        // 表示縦横比正規化
+        viewRatioNormalize.setOnAction(event -> {
+            if(!Settings.AxisNormalize)
+                Settings.ViewRatioNormalize = viewRatioNormalize.isSelected();
+            else
+                viewRatioNormalize.setSelected(true);
+        });
+        // 軸間隔正規化
+        axisNormalize.setOnAction(event -> {
+            boolean state = axisNormalize.isSelected();
+            Settings.AxisNormalize = state;
+            Settings.ViewRatioNormalize = state;
+            viewRatioNormalize.setSelected(state);
+        });
+        // 1ステップの間隔
+        stepVal.textProperty().addListener((obs, oldVal, newVal) -> {
+            Settings.StepVal = Parse.toDouble(newVal, 0.0);
+        });
+    }
+
+}
