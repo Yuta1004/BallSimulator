1d31235c99591f3da0b28c75f699df694ec98ad1
NakagamiYuta <guguru0014@gmail.com>
2020-09-27 00:28:14 +0900

    [fix] removeObject内でcolorTable要素の削除も行うようにした

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -56,6 +56,7 @@ public class Simulator {
     public void removeObject(String id) {
         if(objects.get(id) == null) { return; }
         objects.remove(id);
+        colorTable.remove(id);
     }
 
     /* オブジェクトリストを返す */
